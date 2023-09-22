7710fdcb757e0bf3caf5898d334040b2c7ad3099
dmanh6302 <dmanh6302@gmail.com>
2023-09-22 15:41:35 +0700

    add download cv

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -13,8 +13,8 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
 
         if (this.hash !== "") {
 
@@ -24,15 +24,29 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
+    });
+
+    $("#downloadButton").click(function () {
+        // Create an anchor element
+        var anchor = $("<a>");
+
+        // Set the href attribute to the path of the file you want to download
+        anchor.attr("href", "assets/download/CV_Nguyễn Đức Mạnh.pdf"); // Replace 'cv.pdf' with the actual file name and path
+
+        // Set the download attribute to specify the file name seen by the user when downloading
+        anchor.attr("download", "CV_Nguyễn Đức Mạnh.pdf"); // You can customize the file name here
+
+        // Trigger a click event on the anchor element to start the download
+        anchor[0].click();
     });
 });
 
 // navbar toggle
-$('#nav-toggle').click(function(){
+$('#nav-toggle').click(function () {
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
